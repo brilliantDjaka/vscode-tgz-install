31a1f90e3c04698f499254b3e939d9cf842de625
Brilliant Djaka Irfanuddin Rofiq <brian.rofiq@gmail.com>
2022-06-01 23:57:34 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if ! command -v wget >/dev/null 2>&1 ; then
+    echo "wget is required"
+    exit 1
+fi
+
+rm -rf "/tmp/vscode-tgz-installer"
+
+mkdir "/tmp/vscode-tgz-installer"
+
+
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64" --output-document="/tmp/vscode-tgz-installer/code.tar.gz"
+
+mkdir ~/Apps 2> /dev/null
+
+rm -rf ~/Apps/VSCode-linux-x64
+
+tar zxvf /tmp/vscode-tgz-installer/code.tar.gz -C ~/Apps
+
+rm -f ~/.local/bin/code
+
+ln -s ~/Apps/VSCode-linux-x64/bin/code ~/.local/bin/code
+
+rm -f ~/.local/share/applications/code.desktop
+
+cat > ~/.local/share/applications/code.desktop <<EOF
+
+[Desktop Entry]
+
+# The type as listed above
+Type=Application
+
+# The name of the application
+Name=Visual Studio Code
+
+# A comment which can/will be used as a tooltip
+#Comment=Flash card based learning tool
+
+# The path to the folder in which the executable is run
+Path=/var/home/djaka/
+
+# The executable of the application, possibly with arguments.
+Exec=code
+
+# The name of the icon that will be used to display this entry
+Icon=/var/home/djaka/Apps/VSCode-linux-x64/resources/app/resources/linux/code.png
+
+# Describes whether this application needs to be run in a terminal or not
+Terminal=false
+
+# Describes the categories in which this entry should be shown
+Categories=IDE;Programming
+
+EOF
